b8f3ac09f61f7af1776564cc5c9bf660939cbd49
Lauri Jutila <git@laurijutila.com>
2020-08-20 15:37:30 +0300

    Async subscription spec coverage.

--- spec/cases/async_subscriptions_spec.rb
+++ spec/cases/async_subscriptions_spec.rb
@@ -3,6 +3,55 @@
 require "rails_helper"
 
 describe "async #subscribe" do
+  let(:event_class) { OneModule::TestEvent }
+
+  let(:callable) do
+    ActiveEventStore::TestEventSubscriber = 
+      Module.new do
+        class << self
+          def events
+            @events ||= []
+          end
+
+          def call(event)
+            events << event
+          end
+        end
+      end
+  end
+
+  let(:event) { event_class.new(user_id: 123, user: { name: "Jones" }) }
+
+  after do
+    ActiveEventStore.send(:remove_const, :TestEventSubscriber) if
+      ActiveEventStore.const_defined?(:TestEventSubscriber)
+  end
+
+  it "enqueues job" do
+    ActiveEventStore.subscribe(callable, to: event_class)
+
+    expect do
+      ActiveRecord::Base.transaction do
+        ActiveEventStore.publish(event)
+      end
+    end.to have_enqueued_job(ActiveEventStore::TestEventSubscriber::SubscriberJob).on_queue("events_subscribers")
+  end
+
+  it "calls subscriber when performed", active_job: :perform do
+    ActiveEventStore.subscribe(callable, to: event_class)
+
+    ActiveRecord::Base.transaction do
+      ActiveEventStore.publish(event)
+    end
+
+    expect(callable.events.size).to eq 1
+    expect(callable.events.last.message_id).to eq event.message_id
+    expect(callable.events.last.user).to be_nil
+    expect(callable.events.last.user_id).to eq 123
+  end
+end
+
+describe "async #subscribe" do
   let(:event_class) { ActiveEventStore::TestEvent }
 
   let(:callable) do
--- spec/support/test_events.rb
+++ spec/support/test_events.rb
@@ -12,3 +12,16 @@ module ActiveEventStore
   class AnotherTestEvent < TestEvent
   end
 end
+
+module OneModule
+end
+
+module OneModule
+  class TestEvent < ActiveEventStore::Event
+    self.identifier = "one_module.test_event"
+
+    attributes :user_id, :action_type
+
+    sync_attributes :user
+  end
+end
